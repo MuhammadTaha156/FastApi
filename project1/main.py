4224fa666005dc15173e63478d966b22c0fe95ad
MuhammadTaha156 <toaha155@gmail.com>
2026-07-10 23:01:48 +0500

    new setup

--- project1/main.py
+++ project1/main.py
@@ -2,6 +2,22 @@ from fastapi import FastAPI
 
 app =FastAPI()
 
+# TODOS=[
+#     {
+#     'Title':"Task 1","Description":"My Task1","Is_Competed":False},
+#     {
+#     'Title':"Task 2","Description":"My Task2","Is_Competed":False},
+#     {
+#     'Title':"Task 3","Description":"My Task3","Is_Competed":False},
+#     {
+#     'Title':"Task 4","Description":"My Task4","Is_Competed":False},
+# ]
+
 @app.get('/')
-def hello_world():
-    return "Hello World"
+def home():
+    return {"message":"Hello World"}
+
+# @app.get('/todos/all')
+# def get_All_todos():
+#     return TODOS
+
