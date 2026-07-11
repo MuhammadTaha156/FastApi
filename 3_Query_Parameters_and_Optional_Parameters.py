b2376b27c38c0c93bf2b8a5fb261bce7fd88e9f6
MuhammadTaha156 <toaha155@gmail.com>
2026-07-11 20:27:35 +0500

    Query Paras

--- 3_Query_Parameters_and_Optional_Parameters.py
+++ 3_Query_Parameters_and_Optional_Parameters.py
@@ -3,12 +3,20 @@ from fastapi import FastAPI
 app=FastAPI()
 
 
-
+#Optional Parameters
 @app.get("/users")
 def get_users(name: str=None):
     return {"Name":name}
 
+#Default Parameters values
 @app.get("/products")
 def get_limit(limit: int=10):
     return {"Limit":limit}
+
+#Multiple Query Parameters
+@app.get("/items")
+def get_items(name: str=None, price: int=0):
+    return {"Item":name,
+            "Price":price} 
+
 
