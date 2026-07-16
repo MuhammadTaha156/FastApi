bd854e1fbc06b9ddaf3b54bf9cee84f68ddbc52a
MuhammadTaha156 <toaha155@gmail.com>
2026-07-16 21:20:34 +0500

    put api

--- 6_CRUD_Operations_To_Do_API_Project.py
+++ 6_CRUD_Operations_To_Do_API_Project.py
@@ -11,6 +11,32 @@ class Todo(BaseModel):
     completed:bool
 
 @app.post("/todos")
-def create_todo(todo:Todo):
+def create_todos(todo:Todo):
     todos.append(todo)
     return {"message":"Todo Added","data":todo}
+
+
+@app.get('/todos')
+def get_todos():
+    return todos
+
+@app.get("/todos/{todo_id}")
+def get_todo(todo_id:int):
+    for todo in todos:
+        if todo.id == todo_id:
+            return todo
+        
+    return {"error":"Todo not found"}
+
+
+@app.post("/todos/{todo_id}")
+def update_todo(todo_id:int,updated_todo:Todo):
+    for index,todo in enumerate(todos):
+        if todo.id==todo_id:
+            todos[index]=updated_todo
+            return {
+                "message":"Data Updated",
+                "Data":updated_todo
+            }
+    return {"Error":"Todo not Found"}
+             
