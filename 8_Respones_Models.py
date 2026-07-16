d8db0c2f6d0a1beec1e839459a9a86b140c247fa
MuhammadTaha156 <toaha155@gmail.com>
2026-07-16 22:19:44 +0500

    response model

---
+++ 8_Respones_Models.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app=FastAPI()
+
+class User(BaseModel):
+    name:str
+    age:int
+    password:str
+
+class UserResponse(BaseModel):
+    name:str
+    age:int
+
+@app.get("/users",response_model=UserResponse)
+def get_user():
+    return {
+        "name":"Mohit",
+        "age":24,
+        "password":"12323",
+        "sdfds":213
+    }
+
+
+
+
