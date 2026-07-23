fc9560b9aa776c9e1793c5ece1fb665e34c49c86
MuhammadTaha156 <toaha155@gmail.com>
2026-07-23 20:58:47 +0500

    10_Exception_Handling_And_Global_Error

--- 10_Exception_Handling_And_Global_Error.py
+++ 10_Exception_Handling_And_Global_Error.py
@@ -1,5 +1,55 @@
-from fastapi import FastAPI,status,HTTPException
+from fastapi import FastAPI,status,HTTPException,Request
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 
 app=FastAPI()
+
+# @app.get("/users/{user_id}")
+# def get_user(user_id:int):
+#     if user_id !=1:
+#         raise HTTPException(status_code=404,
+#                             detail="User not Found")
+    
+#     return {"id":1,
+#             "name":"Mohit"}
+
+# #Custom Exception/Error
+# class User_Not_Found_Exception(Exception):
+#     def __init__(self, name:str):
+#         self.name=name
+
+# @app.get("/users/{name}")
+# def get_user(name:str):
+#     if name != "taha":
+#         raise User_Not_Found_Exception(name)
+#     return{
+#         "Name":name
+#     }
+
+
+
+
+
+#Custom Exception/Error
+class User_Not_Found_Exception(Exception):
+    def __init__(self, name:str):
+        self.name=name
+
+@app.exception_handler(User_Not_Found_Exception)
+def user_not_found_handler(request:Request,exc:User_Not_Found_Exception):
+    return JSONResponse(
+        status_code=404,
+        content={
+            "status":"error",
+            "message":f"User {exc.name} not found"
+        }
+    )
+
+@app.get("/users/{name}")
+def get_user(name:str):
+    if name != "taha":
+        raise User_Not_Found_Exception(name)
+    return{
+        "Name":name
+    }
 
